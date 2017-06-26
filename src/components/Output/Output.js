258b4d8f6da26df95d859e83172bd4f6c6e94841
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-06-25 19:20:49 -0600

    Added functionality to copy to clipboard.

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -5,6 +5,8 @@ import 'codemirror/mode/xml/xml';
 import 'codemirror/lib/codemirror.css';
 import './Output.css';
 
+import Button from '../Button/Button';
+
 class Output extends Component {
     constructor(props) {
         super(props);
@@ -17,7 +19,7 @@ class Output extends Component {
             }
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidUpdate(newProps) {
@@ -25,20 +27,34 @@ class Output extends Component {
         cm.getDoc().setValue(this.props.formattedCode);
     }
 
-    handleChange = newCode => {
-        const cm = this.cm.getCodeMirror();
-        cm.getDoc().setValue(this.props.formattedCode);
+    handleClick(e) {
+        let fakeElem = document.createElement('textarea');
+
+        fakeElem.className = 'sr-only';
+        fakeElem.value = this.props.formattedCode;
+
+        let container = document.getElementById('output');
+        container.appendChild(fakeElem);
+
+        fakeElem.select();
+        
+        document.execCommand('selectAll')
+        document.execCommand('copy', false, null);
+
+        fakeElem.parentElement.removeChild(fakeElem);
     }
 
     render() {
         return (
-            <div className="output">
+            <div className="output" id="output">
                 <h2>Output</h2>
                 <CodeMirror
                     ref={(cm) => this.cm = cm}
                     value={this.props.formattedCode}
                     options={this.state.options}
                 />
+                <br />
+                <Button onClick={this.handleClick}>Copy</Button>
             </div>
         );
     }
