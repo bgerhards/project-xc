59eb1b769b27b96fbc6013b2de8c10dd18199e20
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-06-25 18:29:03 -0600

    Added additional unit tests for Button component.

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -14,4 +14,25 @@ describe('Button', () => {
         
         expect(wrapper).toIncludeText('Foobar');
     });
+
+    it('disables when disabled prop true', () => {
+        const wrapper = shallow(<Button disabled={true}>Foobar</Button>);
+
+        expect(wrapper).toBeDisabled();
+    });
+
+    it('enables by default', () => {
+        const wrapper = shallow(<Button>Foobar</Button>);
+
+        expect(wrapper).not.toBeDisabled();
+    });
+
+    it('executes onClick prop handler on click', () => {
+        const mockOnClick = jest.fn();
+        const wrapper = shallow(<Button onClick={mockOnClick}>Foobar</Button>);
+        
+        wrapper.simulate('click');
+
+        expect(mockOnClick).toHaveBeenCalledTimes(1);
+    });
 });
