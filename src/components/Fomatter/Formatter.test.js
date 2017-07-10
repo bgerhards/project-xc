74526752e700b8cb4ea3f23a82030a7721f98292
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-07-10 09:42:56 -0600

    Updated some tests for Formatter and Output.

--- src/components/Fomatter/Formatter.test.js
+++ src/components/Fomatter/Formatter.test.js
@@ -10,42 +10,4 @@ describe('Formatter', () => {
     it('renders without crashing', () => {
         shallow(<Formatter />);
     });
-
-    it('minifies code', () => {
-        const originalCode = `
-            <foo>
-                <bar>bar1</bar>
-                        <bar>bar2</bar>
-            </foo>`;
-
-        const wrapper = shallow(<Formatter />);
-        const instance = wrapper.instance();
-
-        instance.setState({ originalCode });
-
-        const formattedCode = '<foo><bar>bar1</bar><bar>bar2</bar></foo>';
-
-        instance.handleMinifyClick();
-
-        expect(instance.state.formattedCode).toEqual(formattedCode);
-    });
-
-    it('formats code', () => {
-        const originalCode = `
-            <foo>
-                <bar>bar1</bar>
-                        <bar>bar2</bar>
-            </foo>`;
-
-        const wrapper = shallow(<Formatter />);
-        const instance = wrapper.instance();
-
-        instance.setState({ originalCode });
-
-        const formattedCode = pd.xml(originalCode.trim());
-
-        instance.handleFormatClick();
-
-        expect(instance.state.formattedCode).toEqual(formattedCode);
-    });
 });
--- src/components/Output/Output.test.js
+++ src/components/Output/Output.test.js
@@ -3,8 +3,23 @@ import { shallow } from 'enzyme';
 import Output from './Output';
 
 describe('Output', () => {
+    let outputOptions = {
+        lineNumbers: true,
+        mode: 'xml',
+        theme: 'material',
+        matchTags: {
+            bothTags: true
+        }
+    };
 
     it('renders without crashing', () => {
-        shallow(<Output />);
+        shallow(<Output options={outputOptions} />);
+    });
+
+    it('renders with mode selected', () => {
+        const wrapper = shallow(<Output options={outputOptions} />);
+        const select = wrapper.find('select');
+
+        expect(select).toHaveValue('xml');
     });
 });
